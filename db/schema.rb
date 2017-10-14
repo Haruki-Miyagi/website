24c29bee0d620cbf5e1cd1dd92677fdbd299cfce
Haruki-Miyagi <Haruki.Miyagi1112@gmail.com>
2017-10-14 19:24:11 +0900

    add Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013060630) do
+ActiveRecord::Schema.define(version: 20171014102053) do
 
   create_table "articles", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
