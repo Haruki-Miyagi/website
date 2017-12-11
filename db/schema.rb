296267833f619d64fb6b0451c46d887fedbbcc00
Haruki <haruki.miyagi1112@gmail.com>
2017-12-12 00:49:29 +0900

    railsチュートリアル第二章_Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211145606) do
+ActiveRecord::Schema.define(version: 20171211150732) do
 
   create_table "articles", force: :cascade do |t|
     t.string "content"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20171211145606) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
