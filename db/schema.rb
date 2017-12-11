a4213c998d41fa5e6f3383e6e3c4b5cc10f1f9b2
Haruki <haruki.miyagi1112@gmail.com>
2017-12-12 00:06:59 +0900

    railsチュートリアル第二章_Userリソース

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014102606) do
+ActiveRecord::Schema.define(version: 20171211145606) do
 
   create_table "articles", force: :cascade do |t|
     t.string "content"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20171014102606) do
     t.integer "category_id"
     t.string "name"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
