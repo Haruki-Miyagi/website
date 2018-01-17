2a3e54be61d4a6b5f9ae6d8dd3b6b672bb6fe79d
Haruki <haruki.miyagi1112@gmail.com>
2018-01-17 19:40:16 +0900

    Fix update And Add destroy

--- myblog2/app/controllers/posts_controller.rb
+++ myblog2/app/controllers/posts_controller.rb
@@ -27,11 +27,17 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update
+    if @post.update(posts_params)
       redirect_to posts_path
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
 
