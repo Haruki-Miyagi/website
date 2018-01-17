e97e9d15ce075ab94439b3dac8c0cf455c86d15a
Haruki <haruki.miyagi1112@gmail.com>
2018-01-17 21:08:56 +0900

    Add scroll with js

--- myblog2/app/assets/javascripts/application.js
+++ myblog2/app/assets/javascripts/application.js
@@ -12,4 +12,25 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+$(function(){
+  $('#back-to-top').hide();
+
+  $(window).scroll(function(){
+    $('#pos').text($(this).scrollTop());
+    if ($(this).scrollTop() > 600) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  $('#back-to-top a').click(function(){
+    $('body').animate({
+      scrollTop:0
+    }, 5000);
+  });
+});
