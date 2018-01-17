c78ac14c4105f22f4734325f4064b3c490704057
Haruki <haruki.miyagi1112@gmail.com>
2018-01-17 18:44:53 +0900

    title詳細page And Createpage

--- myblog2/app/controllers/posts_controller.rb
+++ myblog2/app/controllers/posts_controller.rb
@@ -4,6 +4,30 @@ class PostsController < ApplicationController
     @posts = Post.all.order(created_at: :desc)
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 
   
+
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :body)
+    end
+
+
 end
