c3e015f9f15d2d2573e0a8ce8e35dddd5d5364ad
Haruki <haruki.miyagi1112@gmail.com>
2018-01-18 21:37:26 +0900

    編集機能(post)

--- myblog3/app/controllers/posts_controller.rb
+++ myblog3/app/controllers/posts_controller.rb
@@ -8,5 +8,41 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(posts_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :body)
+    end
+    
 end
