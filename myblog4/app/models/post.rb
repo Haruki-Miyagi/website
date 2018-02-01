905b2003fa0f34d9ea02783bd8c6654ac49cbdcc
haruki <Haruki.Miyagi1112@gmail.com>
2018-02-01 15:39:51 +0900

    created New comment

--- myblog4/app/models/post.rb
+++ myblog4/app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length: {minimum: 3, message: 'minimum is 3 characters'}
   validates :body, presence: true
 end
