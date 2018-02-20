e096dd98f43337977a7872c2407f325304805bf1
haruki <Haruki.Miyagi1112@gmail.com>
2018-02-20 18:39:05 +0900

    新規作成ページの作成 /myblog5

--- myblog5/app/controllers/posts_controller.rb
+++ myblog5/app/controllers/posts_controller.rb
@@ -8,4 +8,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
