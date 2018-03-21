c25e110b6b4bbdee700bdbf2256388a764cb27a0
haruki <Haruki.Miyagi1112@gmail.com>
2018-03-21 17:15:57 +0900

    userの紐付け se3:70

--- Pictures-test/app/controllers/posts_controller.rb
+++ Pictures-test/app/controllers/posts_controller.rb
@@ -1,28 +1,30 @@
 class PostsController < ApplicationController
-before_action :set_post, only: [:show, :edti, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edti, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def edit
   end
 
   def create
+    @post = current_user.posts.build(post_params)
     # @post = Post.new(post_params)
     # if @post.save
-    if @post = Post.create(post_params)
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your post could not be created! Please check the form"
+      flash[:alert] = "Your post could not be created! Please check the form"
       render :new
     end
   end
