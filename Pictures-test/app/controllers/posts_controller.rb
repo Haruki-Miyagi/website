2471faf02296a6f305db8d0eb49224668c95cb8a
haruki <Haruki.Miyagi1112@gmail.com>
2018-03-21 15:07:36 +0900

    編集機能,削除機能

--- Pictures-test/app/controllers/posts_controller.rb
+++ Pictures-test/app/controllers/posts_controller.rb
@@ -12,13 +12,32 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to posts_path
     else
       render :new
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
