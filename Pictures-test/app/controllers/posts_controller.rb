41d373c4f5a8c2a542fd1650db303cce65f2671f
haruki <Haruki.Miyagi1112@gmail.com>
2018-03-21 15:35:33 +0900

    Add devise-gem

--- Pictures-test/app/controllers/posts_controller.rb
+++ Pictures-test/app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :edti, :update, :destroy]
 
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -13,36 +13,43 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.new(post_params)
-    if @post.save
+    # @post = Post.new(post_params)
+    # if @post.save
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
+      flash.now[:alert] = "Your post could not be created! Please check the form"
       render :new
     end
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
+      flash[:success] = "Your post has been update!"
       redirect_to posts_path
     else
+      flash.now[:alert] = "Your post could not be updated! Please check the form"
       render :edit
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    flash[:success] = "Your post has been deleted!"
+    redirect_to root_path
   end
 
   private
     def post_params
       params.require(:post).permit(:title, :image)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 
 end
